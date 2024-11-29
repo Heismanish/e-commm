e1e9801d3ad2373a786e2a7aa7138a3b8c839b7f
Manish Kumar Gupta <manishgu231@gmail.com>
2024-11-29 11:09:24 +0530

    INIT: project initialised

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <p className="text-3xl text-red-500 ">Hello world</p>
+      <div>
+        <p className="text-3xl text-red-500">Hello</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
